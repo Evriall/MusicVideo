591bcb77b8c999c7f12898a3f440e830e1eafc72
Sergey Guznin <sergey.huz@gmail.com>
2016-02-27 21:19:52 +0200

    Added detail view controller.

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Sergey Guznin on 27.02.16.
+//  Copyright © 2016 Sergey Guznin. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
